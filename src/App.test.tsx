93bb1634f789635a86002296c24af8d287ab3026
Merzaad <Merzaad@gmail.com>
2022-02-08 07:48:31 +0330

    npx create-react-app x --template redux-typescript

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+/* eslint-disable no-undef */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+import App from './App'
 
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
-    </Provider>
-  );
+    </Provider>,
+  )
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+  expect(getByText(/learn/i)).toBeInTheDocument()
+})
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,19 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
-});
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   RootState,
   unknown,
   Action<string>
->;
+>
