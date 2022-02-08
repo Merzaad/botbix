532fca29c8e3816b4a12da122643af79ce6a42db
Merzaad <Merzaad@gmail.com>
2022-02-08 16:35:13 +0330

    Router

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import { store } from './app/store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
